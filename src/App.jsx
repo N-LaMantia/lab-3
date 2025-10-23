7d33055e902b31ecdb7305d3411aaadc97961b71
Nicholas LaMantia <nicholas.lamantia@snhu.edu>
2025-10-23 14:48:11 -0400

    CSS, HTML, added JSon files, added js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,115 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { mathButtonClick } from './genTopics';
+import { historyButtonClick } from './genTopics';
+import { scienceButtonClick } from './genTopics';
+import { literatureButtonClick } from './genTopics';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [viewMathTopics, setViewMathTopics] = useState(false)
+  const [viewHistoryTopics, setViewHistoryTopics] = useState(false)
+  const [viewScienceTopics, setViewScienceTopics] = useState(false)
+  const [viewLiteratureTopics, setViewLiteratureTopics] = useState(false)
 
+
+  //functions to handle general button clicks 
+  function handleMathClick() {
+    mathButtonClick();
+    setViewMathTopics(true);
+    setViewHistoryTopics(false);
+    setViewScienceTopics(false);
+    setViewLiteratureTopics(false);
+  }
+
+  function handleHistoryClick() {
+    historyButtonClick();
+    setViewHistoryTopics(true);
+    setViewMathTopics(false);
+    setViewScienceTopics(false);
+    setViewLiteratureTopics(false);
+  }
+
+  function handleScienceClick() {
+    scienceButtonClick();
+    setViewScienceTopics(true);
+    setViewMathTopics(false);
+    setViewHistoryTopics(false);
+    setViewLiteratureTopics(false);
+  }
+
+  function handleLiteratureClick() {
+    literatureButtonClick();
+    setViewLiteratureTopics(true);
+    setViewMathTopics(false);
+    setViewHistoryTopics(false);
+    setViewScienceTopics(false);
+  }
+
+
+  //RETURN STATEMENT
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="page-title">Welcome to Quick Quizzes!</h1>
+      <br></br>
+      <br></br>
+      <div className="topic-container">
+        <button className="math-button" onClick={handleMathClick}>Math</button>
+        <button className="history-button" onClick={handleHistoryClick}>History</button>
+        <button className="science-button" onClick={handleScienceClick}>Science</button>
+        <button className="literature-button" onClick={handleLiteratureClick}>Literature</button>
+      </div >
+      {viewMathTopics && (
+        <>
+          <h1 className="math-title">Math Topics</h1>
+          <br></br>
+          <br></br>
+          <div className="math-topic-container">
+
+            <button className="Calculus Quiz">Math</button>
+
+            <button className="Algebra Quiz">Algebra</button>
+
+            <button className="Geometry Quiz">Geometry</button>
+          </div >
+        </>
+      )}
+      {viewHistoryTopics && (
+        <>
+          <h1 className="history-title">History Topics</h1>
+          <br></br>
+          <br></br>
+          <div className="history-topic-container">
+            <button className="Canada History Quiz">Ancient History</button>
+            <button className="USA History Quiz">Medieval History</button>
+            <button className="Japan History Quiz">Modern History</button>
+          </div >
+        </>
+      )}
+      {viewScienceTopics && (
+        <>
+          <h1 className="science-title">Science Topics</h1>
+          <br></br>
+          <br></br>
+          <div className="science-topic-container">
+            <button className="Physics Quiz">Physics</button>
+            <button className="Chemistry Quiz">Chemistry</button>
+            <button className="Biology Quiz">Biology</button>
+          </div >
+        </>
+      )}
+      {viewLiteratureTopics && (
+        <>
+          <h1 className="literature-title">Literature Topics</h1>
+          <br></br>
+          <br></br>
+          <div className="literature-topic-container">
+            <button className="Poetry Quiz">Poetry</button>
+            <button className="Novel Quiz">Novels</button>
+            <button className="Play Quiz">Drama</button>
+          </div >
+        </>
+      )}
     </>
   )
 }
